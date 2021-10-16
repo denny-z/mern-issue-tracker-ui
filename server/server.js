46ad2533678468224370d8364536c44740dd0209
denny-z <denys.kudryashov@gmail.com>
2021-10-17 01:48:23 +0300

    Part 5 - GraphQL About API

--- server/server.js
+++ server/server.js
@@ -1,9 +1,35 @@
 const express = require('express');
+const fs = require('fs');
+const { ApolloServer } = require('apollo-server-express');
 
 const app = express();
 const filesMiddleware = express.static('public');
 app.use('/', filesMiddleware);
 
-app.listen(3000, function() {
+const typeDefs = fs.readFileSync('./server/schema.graphql', 'utf-8');
+
+let aboutMessage = 'Hello GraphQL world!';
+
+function setAboutMessage(_, { message }) {
+  return aboutMessage = message;
+};
+
+const resolvers = {
+  Query: {
+    about: () => aboutMessage,
+  },
+  Mutation: {
+    setAboutMessage,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen(3000, function () {
   console.log('app started');
 });
