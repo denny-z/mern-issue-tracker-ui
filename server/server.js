3a548e63cf4a8cab4f95a1897dc30fa289ac8178
denny-z <denys.kudryashov@gmail.com>
2021-10-17 02:00:11 +0300

    Part 5 - Adding "Issues list" to GraphQL

--- server/server.js
+++ server/server.js
@@ -14,9 +14,30 @@ function setAboutMessage(_, { message }) {
   return aboutMessage = message;
 };
 
+const initialIssues = [
+  {
+    id: 1,
+    status: 'New',
+    owner: 'Ravan',
+    effort: 5,
+    created: new Date('2018-08-15'),
+    due: undefined,
+    title: 'Error in console when clicking Add',
+  },
+  {
+    id: 2,
+    status: 'Assigned',
+    owner: 'Eddie',
+    effort: 14,
+    created: new Date('2018-08-16'), due: new Date('2018-08-30'),
+    title: 'Missing bottom border on panel',
+  }
+];
+
 const resolvers = {
   Query: {
     about: () => aboutMessage,
+    issuesList: () => initialIssues,
   },
   Mutation: {
     setAboutMessage,
