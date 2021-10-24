fd0495f84dea6c06e8748993e3c0183db37c2a4f
denny-z <denys.kudryashov@gmail.com>
2021-10-24 14:00:15 +0300

    Part 6 - Remove not in use variable

--- server/server.js
+++ server/server.js
@@ -64,26 +64,6 @@ async function addIssue(_, { issue }) {
   return savedIssue;
 };
 
-const issuesDB = [
-  {
-    id: 1,
-    status: 'New',
-    owner: 'Ravan',
-    effort: 5,
-    created: new Date('2018-08-15'),
-    due: undefined,
-    title: 'Error in console when clicking Add',
-  },
-  {
-    id: 2,
-    status: 'Assigned',
-    owner: 'Eddie',
-    effort: 14,
-    created: new Date('2018-08-16'), due: new Date('2018-08-30'),
-    title: 'Missing bottom border on panel',
-  }
-];
-
 const resolvers = {
   Query: {
     about: () => aboutMessage,
