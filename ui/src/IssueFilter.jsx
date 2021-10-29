0807dfee11c17e346cdceba4cf96d9977bbb4581
denny-z <denys.kudryashov@gmail.com>
2021-10-29 13:46:25 +0300

    Part 9 - Add dropdown to IssueFilter

--- ui/src/IssueFilter.jsx
+++ ui/src/IssueFilter.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 // eslint-disable-next-line react/prefer-stateless-function
-export default class IssueFilter extends React.Component {
+class IssueFilter extends React.Component {
+  constructor() {
+    super();
+    this.onStatusChange = this.onStatusChange.bind(this);
+  }
+
+  onStatusChange(e) {
+    const status = e.target.value;
+    const { history } = this.props;
+
+    history.push({
+      path: '/issues',
+      search: status ? `?status=${status}` : '',
+    });
+  }
+
   render() {
     return (
       <div>
-        <Link to="/issues">All Issues</Link>
-        {' | '}
-        <Link to={{ path: '/issues', search: '?status=New' }}>New Issues</Link>
-        {' | '}
-        <Link to={{ path: '/issues', search: '?status=Assigned' }}>Assigned Issues</Link>
+        Status
+        {' '}
+        <select onChange={this.onStatusChange}>
+          <option value="">All</option>
+          <option value="New">New</option>
+          <option value="Assigned">Assigned</option>
+          <option value="Closed">Closed</option>
+        </select>
       </div>
     );
   }
 }
+
+export default withRouter(IssueFilter);
