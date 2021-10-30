92a3dc64346439265e801f8ad704d6fd9d06d6db
denny-z <denys.kudryashov@gmail.com>
2021-10-31 01:56:18 +0300

    Part 10 - "Apply" and "Reset"  buttons for IssueFilter

--- ui/src/IssueFilter.jsx
+++ ui/src/IssueFilter.jsx
@@ -4,13 +4,42 @@ import URLSearchParams from 'url-search-params';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class IssueFilter extends React.Component {
-  constructor() {
+  constructor({ location: { search } }) {
     super();
+    const params = new URLSearchParams(search);
+    this.state = {
+      status: (params.get('status') || ''),
+      isChanged: false,
+    };
+
     this.onStatusChange = this.onStatusChange.bind(this);
+    this.applyFilter = this.applyFilter.bind(this);
+    this.showOriginalFilter = this.showOriginalFilter.bind(this);
+  }
+
+  componentDidUpdate(prevProps) {
+    const { location: { search: prevSearch } } = prevProps;
+    const { location: { search } } = this.props;
+    if (prevSearch !== search) {
+      this.showOriginalFilter();
+    }
   }
 
   onStatusChange(e) {
-    const status = e.target.value;
+    this.setState({ status: e.target.value, isChanged: true });
+  }
+
+  showOriginalFilter() {
+    const { location: { search } } = this.props;
+    const params = new URLSearchParams(search);
+    this.setState({
+      status: params.get('status') || '',
+      isChanged: false,
+    });
+  }
+
+  applyFilter() {
+    const { status } = this.state;
     const { history } = this.props;
 
     history.push({
@@ -20,19 +49,22 @@ class IssueFilter extends React.Component {
   }
 
   render() {
-    const { location: { search } } = this.props;
-    const params = new URLSearchParams(search);
+    const { status, isChanged } = this.state;
 
     return (
       <div>
         Status
         {' '}
-        <select value={params.get('status') || ''} onChange={this.onStatusChange}>
+        <select value={status} onChange={this.onStatusChange}>
           <option value="">All</option>
           <option value="New">New</option>
           <option value="Assigned">Assigned</option>
           <option value="Closed">Closed</option>
         </select>
+        {' | '}
+        <button type="button" onClick={this.applyFilter}>Apply</button>
+        {' | '}
+        <button type="button" onClick={this.showOriginalFilter} disabled={!isChanged}>Reset</button>
       </div>
     );
   }
