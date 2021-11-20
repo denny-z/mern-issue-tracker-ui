d14d51a4328c8a66d0f1707c6e06529ff47423fa
denny-z <denys.kudryashov@gmail.com>
2021-11-20 16:42:46 +0200

    Part 7 - Enable CORS for API server, apply CORS fixes for UI server

--- api/api_handler.js
+++ api/api_handler.js
@@ -34,7 +34,9 @@ const server = new ApolloServer({
 });
 
 function installHandler(app) {
-  server.applyMiddleware({ app, path: '/graphql' });
+  const enableCors = (process.env.ENABLE_CORS || 'true') === 'true';
+  console.log('CORS setting:', enableCors);
+  server.applyMiddleware({ app, path: '/graphql', cors: enableCors });
 }
 
 module.exports = { installHandler };
