e01814021252fa3ef0de2eec5a764504b20c214e
denny-z <denys.kudryashov@gmail.com>
2021-12-13 02:31:38 +0200

    [react-redux] DOC: Place TODOs with bugs to fix

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -209,7 +209,7 @@ export function issueDelete(id, showError, onSuccess) {
   };
 }
 
-// TODO: [react-redux] fix issue does not appear.
+// TODO: [react-redux] fix that issue does not appear.
 // Steps:
 // 1. Delete issue.
 // 2. Restore issue from Toast by clicking "Undo".
@@ -229,6 +229,12 @@ export function issueRestore(id, showError, showSuccessWithMessage) {
   };
 }
 
+// TODO: [react-redux] fix that issue does not appear.
+// Steps:
+// 1. Go to last page of IssueList.
+// 2. Create issue (via + in nav bar).
+// Actual result: Issue does not appear in issues list.
+// Expected result: Issue will appear (at least at the bottom of current list).
 export function issueCreate(issue, showError, onSuccess) {
   return async (dispatch) => {
     const data = await graphQLFetch(ISSUE_CREATE_QUERY, { issue }, showError);
